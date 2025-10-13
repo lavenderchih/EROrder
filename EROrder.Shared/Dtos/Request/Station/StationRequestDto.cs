b4b3d93f4e87a7cfc5e04634c78bd4c69f6d4eb8
Lavender <lavender111541@gmail.com>
2025-10-13 16:06:28 +0800

    護理站dto

---
+++ EROrder.Shared/Dtos/Request/Station/StationRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace EROrder.Shared.Dtos.Request.Station
+{
+    public class StationRequestDto
+    {
+        public string Station { get; set; } = string.Empty;
+        public string StationName { get; set; } = string.Empty;
+    }
+}
